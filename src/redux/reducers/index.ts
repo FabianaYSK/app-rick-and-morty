dcf9d1a56af6756c3d210406790b32775d2d2204
FabianaYSK <yumifabi.s@gmail.com>
2023-06-27 17:07:14 -0300

    Adicionada lógica para manter o status de favorito

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,11 @@
-import { Action } from "../actions/types";
+// import { Action } from "../actions/types";
+import { IActionFavoritos, IInitialState } from "../actions/types";
 
-const INITIAL_STATE = {
-  personagem: [],
+const INITIAL_STATE: IInitialState = {
+  favoritos: {},
 };
 
-export function personagemReducer(state = INITIAL_STATE, action: Action) {
+/* export function personagemReducer(state = INITIAL_STATE, action: Action) {
   switch (action.type) {
     case "FETCH_PERSONAGEM": {
       return {
@@ -14,5 +15,25 @@ export function personagemReducer(state = INITIAL_STATE, action: Action) {
     }
     default: return state;
   }
+} */
 
-}
+export const favoritosReducer = (state = INITIAL_STATE, action: IActionFavoritos) => {
+  switch (action.type) {
+    case 'SET_FAVORITO':
+      return {
+        ...state,
+        favoritos: action.payload,
+      };
+    case 'HANDLE_FAVORITO':
+      return {
+        ...state,
+        favoritos: {
+          ...state.favoritos,
+          [action.payload]: !state.favoritos[action.payload],
+        },
+      };
+    default:
+      return state;
+  }
+};
+
